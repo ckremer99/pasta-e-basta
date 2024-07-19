171a540ba61601b8c417191b56e739819b7a3fda
Cody Kremer <ckremer99@outlook.com>
2024-07-19 11:18:16 -0700

    added sign-in functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,4 +12,60 @@ router.get('/sign-in', (req, res) => {
     res.render('auth/sign-in.ejs')
 })
 
+router.post('/sign-up', async (req, res) => {
+    try {
+        const userInDatabase = await User.findOne( {username: req.body.username });
+
+        if(userInDatabase) {
+            return res.send('Username already taken')
+        }
+
+        if (req.body.password !== req.body.confirmPassword) {
+            return res.send('Pasword and Confirm Password Must Match')
+        }
+
+        const hashedPassword = bcrypt.hashSync(req.body.password, 10); 
+        req.body.password = hashedPassword; 
+
+        await User.create(req.body)
+        res.redirect('/auth/sign-in')
+
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+})
+
+router.post('/sign-in', async (req, res) => {
+    try {
+        const userInDatabase = await User.findOne({ username: req.body.username })
+        if (!userInDatabase) {
+            return res.send('Login failed. Please try again.')
+        }
+
+        const validPassword = bcrypt.compareSync(
+            req.body.password, 
+            userInDatabase.password
+        )
+
+        if (!validPassword) {
+            return res.send('Login failed. Please try again.')
+        }
+
+        req.session.user = {
+            username: userInDatabase.username,
+            _id: userInDatabase._id
+        }
+
+        res.redirect('/')
+
+    } catch (error) {
+        console.log(error); 
+        res.redirect('/')
+    }
+    
+
+   
+})
+
 module.exports = router;
