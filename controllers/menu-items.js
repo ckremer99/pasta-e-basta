15aadc3bce9f8896afc19127cb4a43ee9021c743
Cody Kremer <ckremer99@outlook.com>
2024-07-19 21:46:09 -0700

    added edit links and page

--- controllers/menu-items.js
+++ controllers/menu-items.js
@@ -1,19 +1,37 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
 
 const User = require('../models/user.js')
 
-router.get('/:userId/new', async (req, res) => {
-
-    res.render('menu-items/new', { userId: req.params.userId })
+router.get('/:userId/new', (req, res) => {
+    res.render('menu-items/new.ejs', { userId: req.params.userId })
 })
+
+router.get('/:userId/:itemId/edit', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.params.userId);
+        const itemId = new mongoose.Types.ObjectId(req.params.itemId); 
+        const currentItem = currentUser.menuItems.find(item => {
+            return item._id.equals(itemId); 
+        });
+        res.render('menu-items/edit.ejs', {
+            currentItem,
+            userId: req.params.userId
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
 
 router.get('/:userId/full-menu', async (req, res) => {
     try {
         const currentUser = await User.findById(req.params.userId)
         const fullMenu = currentUser.menuItems;
         res.render('menu-items/full-menu.ejs', {
-           fullMenu
+           fullMenu,
+           user: currentUser
         })
     } catch (error) {
         console.log(error)
@@ -29,7 +47,44 @@ router.get('/:userId/pizzas', async (req, res) => {
             return item.category === 'Pizza'
         })
         res.render('menu-items/pizzas.ejs', {
-            pizzas
+            pizzas,
+            user: currentUser
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+})
+
+router.get('/:userId/pastas', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.params.userId)
+        const fullMenu = currentUser.menuItems;
+        const pastas = fullMenu.filter(item => {
+            return item.category === 'Pasta'
+        })
+        res.render('menu-items/pastas.ejs', {
+            pastas,
+            user: currentUser
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+})
+
+router.get('/:userId/desserts', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.params.userId)
+        const fullMenu = currentUser.menuItems;
+        const desserts = fullMenu.filter(item => {
+            return item.category === 'Dessert'
+        })
+        res.render('menu-items/desserts.ejs', {
+            desserts,
+            user: currentUser
         })
 
     } catch (error) {
